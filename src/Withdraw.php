190f288df36a8f6a87318790f91b5e49452674bf
shihan <1609763063@qq.com>
2019-03-07 16:06:35 +0800

    init composer packagist

---
+++ src/Withdraw.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 16097
+ * Date: 2019/3/7
+ * Time: 15:13
+ */
+namespace shwithdraw;
+class Withdraw
+{
+    public static function get()
+    {
+        return '111111111';
+    }
+}
